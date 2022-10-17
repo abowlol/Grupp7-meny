239c274ac7d3f318205bb6e6c50b4e1f0cdad727
iceotop <tais22ls@student.ju.se>
2022-10-17 16:37:41 +0200

    Preliminära Funktioner

--- script.js
+++ script.js
@@ -2,8 +2,102 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var menu = JSON.parse(xhttp.responseText);
-       console.log(menu.meals); //Loggar objekten som ligger is object.json
+       //console.log(menu.meals); //Loggar objekten som ligger is object.json
        var meals = menu.meals; //För att enkelt nå menyn genom meals.objekt
+
+       function generalCall () {
+            
+            meals.forEach((meal) => {
+                
+                const menuDiv = document.querySelector("#menu-container");
+                const mealDiv = document.createElement("div");
+                menuDiv.appendChild(mealDiv);
+                mealDiv.innerHTML = meal.name + ' ' + meal.price + "kr" + '<br>' + meal.description;
+                
+            })
+
+        }
+       //GeneralCall () <---------------------------------- CALL THIS FUNCTION
+       function filterGluten () {
+
+            meals.forEach((meal) => {
+                    
+                if (!(meal.alergies.includes('gluten'))) {
+                    console.log(meal);
+                    const menuDiv = document.querySelector("#menu-container");
+                    const mealDiv = document.createElement("div");
+                    menuDiv.appendChild(mealDiv);
+                    mealDiv.innerHTML = meal.name + ' ' + meal.price + "kr" + '<br>' + meal.description;
+                }
+
+            })
+        
+        }
+        //filterGluten () <---------------------------------- CALL THIS FUNCTION
+        function filterLactose () {
+
+            meals.forEach((meal) => {
+                    
+                if (!(meal.alergies.includes('lactose'))) {
+                    console.log(meal);
+                    const menuDiv = document.querySelector("#menu-container");
+                    const mealDiv = document.createElement("div");
+                    menuDiv.appendChild(mealDiv);
+                    mealDiv.innerHTML = meal.name + ' ' + meal.price + "kr" + '<br>' + meal.description;
+                }
+
+            })
+        
+        }
+        //filterLactose () <---------------------------------- CALL THIS FUNCTION
+        function filterSeafood () {
+
+            meals.forEach((meal) => {
+                    
+                if (!(meal.alergies.includes('seafood'))) {
+                    console.log(meal);
+                    const menuDiv = document.querySelector("#menu-container");
+                    const mealDiv = document.createElement("div");
+                    menuDiv.appendChild(mealDiv);
+                    mealDiv.innerHTML = meal.name + ' ' + meal.price + "kr" + '<br>' + meal.description;
+                }
+
+            })
+        
+        }
+        //filterSeafood () <---------------------------------- CALL THIS FUNCTION 
+        function filterEggs () {
+
+            meals.forEach((meal) => {
+                    
+                if (!(meal.alergies.includes('egg'))) {
+                    console.log(meal);
+                    const menuDiv = document.querySelector("#menu-container");
+                    const mealDiv = document.createElement("div");
+                    menuDiv.appendChild(mealDiv);
+                    mealDiv.innerHTML = meal.name + ' ' + meal.price + "kr" + '<br>' + meal.description;
+                }
+
+            })
+        
+        }
+        //filterEggs () <---------------------------------- CALL THIS FUNCTION 
+        function filterMeat () {
+
+            meals.forEach((meal) => {
+                    
+                if (meal.meatType === 'vegetarian') {
+                    console.log(meal);
+                    const menuDiv = document.querySelector("#menu-container");
+                    const mealDiv = document.createElement("div");
+                    menuDiv.appendChild(mealDiv);
+                    mealDiv.innerHTML = meal.name + ' ' + meal.price + "kr" + '<br>' + meal.description;
+                }
+
+            })
+        
+        }
+        //filterMeat () <---------------------------------- CALL THIS FUNCTION 
     }
 };
 xhttp.open("GET", "object.json", true);
