5984bf6609c918362a6a325850b36aa8a5961b64
Gerdtman <90909306+Gerdtman@users.noreply.github.com>
2022-10-20 16:26:18 +0200

    Update simonScript.js

--- simonScript.js
+++ simonScript.js
@@ -10,6 +10,130 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", "object.json", true);
 xhttp.send();
 
+const menuDiv = document.querySelector("#menu-container");
+
+
+let OnOffSwitchForGluten = false;
+function filterGluten(){
+
+    const glutenDivs = document.querySelectorAll('.gluten');
+    //console.log(glutenDivs);
+
+
+    if (OnOffSwitchForGluten === false) {
+
+        glutenDivs.forEach((div) => {
+
+            div.classList.toggle("glutenlocked");
+            console.log(div);
+            div.style.display = "none";
+            //console.log(div);
+
+        })
+
+        OnOffSwitchForGluten = true;
+    }
+    else {
+
+        glutenDivs.forEach((div) => {
+            
+            //numbers
+            div.classList.toggle("glutenlocked");
+            //-1
+            console.log(div);
+            checkIfLockedAndReturnDisplay(div);
+            //if locked is found in any of the "div":s, keep display:none; else, display:block
+            //returnDisplay(div);
+            
+        })
+
+        OnOffSwitchForGluten = false;
+
+    }
+
+}
+
+function checkIfLockedAndReturnDisplay (div){
+
+    /*Look inside this div. Is there a substring called "locked" in it? If there isn't
+    such a substring, you'll return -1, meaning it's not locked--meaning it's free
+    to appear (display:block).*/
+
+            const string = String(div.outerHTML);
+            const locked = string.search('locked');
+
+            if (locked === -1){
+    return div.style.display = "block"}
+    
+}
+
+let OnOffSwitchForLactose = false;
+function filterLactose(){
+
+    const lactoseDivs = document.querySelectorAll('.lactose');
+
+
+    if (OnOffSwitchForLactose === false) {
+
+        lactoseDivs.forEach((div) => {
+
+            div.classList.toggle("lactoselocked");
+            div.style.display = "none";
+            console.log(div);
+            
+        })
+
+        OnOffSwitchForLactose = true;
+    }
+    else {
+
+        lactoseDivs.forEach((div) => {
+
+            div.classList.toggle("lactoselocked");
+            console.log(div);
+            checkIfLockedAndReturnDisplay(div);
+            
+        })
+
+        OnOffSwitchForLactose = false;
+
+    }
+
+}
+
+let OnOffSwitchForSeafood = false;
+function filterSeafood(){
+
+    const seafoodDivs = document.querySelectorAll('.seafood');
+
+
+    if (OnOffSwitchForSeafood === false) {
+
+        seafoodDivs.forEach((div) => {
+
+            div.classList.toggle("seafoodlocked");
+            div.style.display = "none";
+            console.log(div);
+            
+        })
+
+        OnOffSwitchForSeafood = true;
+    }
+    else {
+
+        seafoodDivs.forEach((div) => {
+
+            div.classList.toggle("seafoodlocked");
+            console.log(div);
+            checkIfLockedAndReturnDisplay(div);
+            
+        })
+
+        OnOffSwitchForSeafood = false;
+
+    }
+
+}
 
 function SortAscending(){
         var xhttp = new XMLHttpRequest();
@@ -17,13 +141,13 @@ function SortAscending(){
         xhttp.onload = function(){
             var menu = JSON.parse(xhttp.responseText);
 
-                removeElementByClass("mealCard"); 
+            removeElementByClass("mealCard"); 
 
-                var menuSortedAscending = menu.meals.sort((a, b) => {
+            var menuSortedAscending = menu.meals.sort((a, b) => {
                     return a.price - b.price;
-                });
+            });
 
-                printMenu(menuSortedAscending);
+            printMenu(menuSortedAscending);
 
 
         }
@@ -36,13 +160,13 @@ function SortDescending(){
         xhttp.onload = function(){
             var menu = JSON.parse(xhttp.responseText);
 
-                removeElementByClass("mealCard"); 
+            removeElementByClass("mealCard"); 
 
-                var menuSortedAscending = menu.meals.sort((a, b) => {
+            var menuSortedAscending = menu.meals.sort((a, b) => {
                     return b.price - a.price;
                 });
 
-                printMenu(menuSortedAscending);
+            printMenu(menuSortedAscending);
 
 
         }
@@ -75,20 +199,9 @@ function SortDescending(){
 
                 mealCard.innerHTML = 
                 `
-                <div class="card mb-3" style="max-width: 100%;">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img src="${menu[i].image}" class="card-img" alt="${menu[i].name}">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">${menu[i].name}</h5>
-                      <p class="card-text">${menu[i].description}</p>
-                      <p class="card-text">${menu[i].price} kr</p>
-                    </div>
-                  </div>
-                </div>
-                </div>
+                <h3 id="mealCardTitle">${menu[i].name}</h3></br>
+                    ${menu[i].price}kr</br>
+                <i>${menu[i].description}</i>
                 `;
 
             }
